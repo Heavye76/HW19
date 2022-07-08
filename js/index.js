04c7b3b57c09c4d0b086a5719f7fd6fdf490ec4a
Heavye76 <97629126+Heavye76@users.noreply.github.com>
2022-07-08 13:09:46 -0700

    Fading Arrow
    The arrow Fades!

---
+++ js/index.js
@@ -0,0 +1,38 @@
+
+console.log("This thing is working");
+
+// $("#fade").click(function(){
+
+  // $("#fade").fadeOut("slow");
+  
+// });
+
+
+// 
+// $( ".fade" ).click(function() {
+//  $( ".fade" ).fadeOut( 3000 );
+//  });
+
+
+$(document).ready(function(){
+  $( ".fade" ).fadeOut ( 3000 );
+});
+
+
+// $(selector).fadeTo(duration,opacity,callback);
+
+
+$(document).ready(function () {
+  $('#faze').fadeIn(5200); 
+  $('#faze').fadeOut(5200); 
+  $('#faze').fadeIn(5200); 
+  // OR $('div.toshow').show(2200);
+  // OR $('div.toshow').slideDown("slow");
+});
+
+
+// $(document).ready(function () {
+//  $('div.toshow').fadeToggle(5200); 
+//  $('div.toshow').fadeOut(5200);
+//  );
+// });
